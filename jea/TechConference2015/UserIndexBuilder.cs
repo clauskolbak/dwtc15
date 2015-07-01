25e1440d51bc55b581511478e4e84c1785c28816
Jeppe Eriksson Agger <jea@dynamicweb.dk>
2015-07-01 13:58:10 +0200

    Committed source code for the session Indexing Everything
    Committed source code for the session Indexing Everything. Code is
    updated for compatibility with version 8.6.1.13.

---
+++ jea/TechConference2015/UserIndexBuilder.cs
@@ -0,0 +1,99 @@
+﻿using Dynamicweb.Diagnostics.Tasks;
+using Dynamicweb.Indexing;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace TechConference2015
+{
+    public class UserIndexBuilder : IIndexBuilder
+    {
+        public string Action { get; set; }
+        public IDictionary<string, string> Meta { get; set; }
+        public string Name { get; set; }
+        public Dynamicweb.Indexing.Schemas.SchemaDefinition Schema { get; set; }
+        public IDictionary<string, string> Settings { get; set; }
+        public string Type { get; set; }
+        public IEnumerable<IIndexBuilderExtender<IIndexBuilder>> Extenders { get; set; }
+
+        public IEnumerable<string> SupportedActions { get { return new[] { "Full" }; } }
+
+        public void Build(IIndexWriter writer, Dynamicweb.Diagnostics.Tracking.Tracker tracker)
+        {
+            #region Implementation
+            writer.Open(openForAppend:false);
+
+            if (!TaskManager.Context.ContainsKey("Database.ConnectionString"))
+                return;
+
+            using (var conn = new SqlConnection(TaskManager.Context["Database.ConnectionString"].ToString()))
+            {
+                #region Connection
+                conn.Open();
+
+                // Get count
+                using (var cmd = conn.CreateCommand())
+                {
+                    tracker.LogInformation("Getting count");
+                    cmd.CommandText = "SELECT COUNT(AccessUserID) FROM AccessUser WHERE AccessUserType = 5";
+                    tracker.Status.TotalCount = (int) cmd.ExecuteScalar();
+                }
+
+                // Handle users
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText =
+                        "SELECT AccessUserID, AccessUserUserName, AccessUserName, AccessUserEmail, AccessUserNewsletterAllowed, AccessUserGroups FROM AccessUser WHERE AccessUserType = 5";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        tracker.LogInformation("Handling users");
+
+                        while (reader.Read())
+                        {
+                            #region Build document
+                            var doc = new IndexDocument();
+
+                            for (var i = 0; i < reader.FieldCount; i++)
+                            {
+                                var name = reader.GetName(i);
+                                var value = reader.GetValue(i);
+
+                                if (name == "AccessUserGroups")
+                                {
+                                    #region Handle groups
+                                    if (reader.IsDBNull(i))
+                                        continue;
+
+                                    var groupIds = value.ToString().Split(new[] {'@'}, StringSplitOptions.RemoveEmptyEntries);
+
+                                    var groupIdList =
+                                        groupIds
+                                            .Select(id => id.Trim('@'))
+                                            .Where(id => !string.IsNullOrEmpty(id))
+                                            .Select(int.Parse)
+                                            .ToList();
+
+                                    if (groupIdList.Count > 0)
+                                        doc.Add("Groups", groupIdList);
+                                    #endregion
+                                }
+                                else
+                                {
+                                    doc.Add(name, value);
+                                }
+                            }
+
+                            writer.AddDocument(doc);
+                            tracker.IncrementCounter();
+                            #endregion
+                        }
+                    }
+                }
+                #endregion
+            }
+            #endregion
+        }
+    }
+}
---
+++ jea/TechConference2015/UserSchemaExtender.cs
@@ -0,0 +1,82 @@
+﻿using Dynamicweb.Indexing;
+using Dynamicweb.Indexing.Schemas;
+using System.Collections.Generic;
+
+namespace TechConference2015
+{
+    public class UserSchemaExtender : IIndexSchemaExtender
+    {
+        public IEnumerable<FieldDefinitionBase> GetFields()
+        {
+            var result = new List<FieldDefinitionBase>
+            {
+                #region Field definitions
+                new FieldDefinition
+                {
+                    Name = "User ID",
+                    SystemName = "UserID",
+                    Source = "AccessUserID",
+                    Type = "System.Int32",
+                    Stored = true,
+                    Indexed = true,
+                    Analyzed = false
+                },
+                #region More fields
+                new FieldDefinition
+                {
+                    Name = "User name",
+                    SystemName = "UserName",
+                    Source = "AccessUserUserName",
+                    Type = "System.String",
+                    Stored = true,
+                    Indexed = true,
+                    Analyzed = false
+                },
+                new FieldDefinition
+                {
+                    Name = "Name",
+                    SystemName = "Name",
+                    Source = "AccessUserName",
+                    Type = "System.String",
+                    Stored = true,
+                    Indexed = true,
+                    Analyzed = false
+                },
+                new FieldDefinition
+                {
+                    Name = "User e-mail",
+                    SystemName = "UserEmail",
+                    Source = "AccessUserEmail",
+                    Type = "System.String",
+                    Stored = true,
+                    Indexed = true,
+                    Analyzed = false
+                },
+                new FieldDefinition
+                {
+                    Name = "Can send e-mail",
+                    SystemName = "UserEmailAllowed",
+                    Source = "AccessUserNewsletterAllowed",
+                    Type = "System.Boolean",
+                    Stored = true,
+                    Indexed = true,
+                    Analyzed = false
+                },
+                new FieldDefinition
+                {
+                    Name = "Groups",
+                    SystemName = "Groups",
+                    Source = "Groups",
+                    Type = "System.Int32[]",
+                    Stored = true,
+                    Indexed = true,
+                    Analyzed = false
+                }
+                #endregion
+                #endregion
+            };
+
+            return result;
+        }
+    }
+}
